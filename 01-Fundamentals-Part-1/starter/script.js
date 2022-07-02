39e1b5328920adedd19f2265f3855e441ca57a8b
Souleymane Faya LENO <leno.souleymane.faya@gmail.com>
2022-07-02 11:26:34 +0100

    index change commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,12 @@
+alert("Hello World!");
+
+let js = 'amazing';
+
+if (js === 'amazing') alert('JavaScript is FUN');
+
+js = 'boring';
+
+if (js === 'amazing') alert('JavaScript is FUN');
+
+let  sum = 40 + 
+
