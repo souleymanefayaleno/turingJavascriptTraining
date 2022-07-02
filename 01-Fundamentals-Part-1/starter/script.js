b43f65c655ad274917f216036054e821f37b7d6d
Souleymane Faya LENO <leno.souleymane.faya@gmail.com>
2022-07-02 12:21:36 +0100

    script.js commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,64 @@
-alert("Hello World!");
+// create and manipulate variable
+ // don't use (function, new, etc.) for variables names
+ let jonas_matilda = "JM";
+ let $function = 27;
+ let first = 123;
+ let firstToken = 'Souleymane';
+ let PI = 3.1415;
+ 
+ let js = 'amazing';
+ if (js === 'amazing') alert('Javascript is FUN!');
+ 
+ let myFirstJob = "Teacher";
+ let mySecondJob = "AWS Developer";
+ let myCurrentJob = "Big Data Developer";
 
-let js = 'amazing';
+ // Print message with alert():
+ alert('Hello World!');
+ alert(js);
 
-if (js === 'amazing') alert('JavaScript is FUN');
 
-js = 'boring';
+ // https://github.com/jonasschmedtmann/complete-javascript-course
 
-if (js === 'amazing') alert('JavaScript is FUN');
+ 
+ // Print message with console():
+ console.log(40 + 8 + 23 -10);
+ console.log(23.4567);
 
-let  sum = 40 + 
+ console.log("Souleymane Faya");
+ console.log("LENO");
+ 
 
+ let myFirstName = "Souleymane Faya";
+ let myLastName = "LENO";
+
+ console.log(myFirstName);
+ console.log(myLastName);
+
+ console.log(myFirstJob);
+ console.log(mySecondJob);
+ console.log(myCurrentJob);
+
+ // Data Types In JavaScript
+ // 1.Number data type:
+ let age = 23;
+
+ // 2.String data type:
+ let name = "LENO Souleymane Faya";
+
+ // 3.Boolean data type:
+ let fullAge = true;
+
+ // 4.Undefined data type:
+ let children;
+
+ // 5.Null data type
+ let time = null;
+
+ // 6.Symbol(ES2015)
+ let alph = 3.1415;
+
+ // 7.BigInt(ES2020)
+ let val = 21556666666666666666666666666666666664444444n;
+
+// 
